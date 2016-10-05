6b744e36c9eddfb54e7f08f83c36d020ecfb3a12
unknown <shohan4556@gmail.com>
2016-10-05 22:22:12 +0600

    Scene Fading script

---
+++ Scripts/FadeScreen.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeScreen : MonoBehaviour {
+
+    public Texture2D fadeTexture;
+
+    [Range(0.1f,1f)]
+    public float fadespeed;
+    public int drawDepth = -1000;
+
+    private float alpha = 0; // to fade out make it zero 0 
+    private float fadeDir = -1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+    void OnGUI() {
+
+        alpha -= fadeDir * fadespeed * Time.deltaTime; // and it will be negative
+        alpha = Mathf.Clamp01(alpha);
+
+        Color newColor = GUI.color; 
+        newColor.a = alpha;
+
+        GUI.color = newColor;
+
+        GUI.depth = drawDepth;
+
+        GUI.DrawTexture( new Rect(0,0, Screen.width, Screen.height), fadeTexture);
+
+    }
+
+  
+
+}
