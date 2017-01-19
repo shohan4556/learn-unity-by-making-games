c110e518b3af93d3e0e43b0c638b23faa23f5d5b
shohan4556 <shohan4556@gmail.com>
2017-01-19 20:36:23 +0600

    touch controller script name changed

--- Scripts/TouchController.cs
+++ Scripts/TouchController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerTouchController : MonoBehaviour {
+public class TouchController : MonoBehaviour {
 
     private PlayerController theplayer;
    
