781567b76478916e261b10855a7666f03fc6861f
shohan4556 <shohan4556@gmail.com>
2017-04-02 00:33:44 +0600

    showGizmos script and 2D Lihghting update

---
+++ Scripts/ShowGizmos.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+// there are objects to pool
+[System.Serializable]
+public class GizmosColor{
+	public Color color;
+}
+
+
+public class ShowGizmos : MonoBehaviour {
+
+	private bool showGizmos = true;
+	// Icon to draw Gizmos
+	public string gizmoIcon = string.Empty;
+
+	[Range(1,100)]
+	public float Range = 5f;
+
+
+	/// <summary>
+	/// Callback to draw gizmos that are pickable and always drawn.
+	/// </summary>
+	void OnDrawGizmos()
+	{
+		if(!showGizmos){ return;}
+
+		// draw icon 
+		Gizmos.DrawIcon(transform.position,gizmoIcon,true);
+
+		// draw color sphere
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(transform.position,Vector3.one*Range);
+
+		// draw forward direction 
+		Gizmos.color = Color.black;
+		
+		if(Range!=1)
+			Gizmos.DrawLine(transform.position, transform.position + transform.forward*Range);
+		else
+			Gizmos.DrawLine(transform.position, transform.position + transform.forward*Range*2f);	
+
+	}
+	
+}
