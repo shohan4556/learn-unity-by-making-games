29fc92b9d77af80dbe33785f44ae6ff3d1a08d66
shohan4556 <shohan4556@gmail.com>
2017-07-28 15:14:58 +0600

    scene transition script

---
+++ Scripts/MySceneManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour {
+
+    public GameObject LoadingScreen; // an UI image - deactive in hierarchy 
+    public Slider loadingBar; // create a slider prefab
+
+
+    // Use this for initialization
+    void Start () {
+     // if loadingBar and loadingScreen is active in hierarchy deactive
+    }
+
+	// laod scene 
+    public void LoadScene ( int sceneID ) {
+        StartCoroutine (LoadAsync (sceneID));
+
+    }
+
+    IEnumerator LoadAsync ( int sceneID ) {
+        AsyncOperation operation = SceneManager.LoadSceneAsync (sceneID);
+
+        LoadingScreen.SetActive (true);
+
+
+        while (!operation.isDone) {
+            float progress = Mathf.Clamp01 (operation.progress / 0.9f);
+
+            loadingBar.value = progress;
+            yield return null;
+        }
+
+    }
+
+
+	// quite game 
+    public void QuiteGame ( ) {
+        Application.Quit ();
+    }
+
+
+}
