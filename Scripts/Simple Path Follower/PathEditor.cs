4e4a5d583af29629af70967f49692bde44532784
shohan4556 <shohan4556@gmail.com>
2017-09-08 17:46:55 +0600

    simple path follower

---
+++ Scripts/Simple Path Follower/PathEditor.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathEditor : MonoBehaviour {
+
+	public Color lineColor = Color.white;
+	public List<Transform> nodes = new List<Transform>();
+	private int segmentCount = 10;
+	
+	
+	/// <summary>
+	/// Callback to draw gizmos that are pickable and always drawn.
+	/// </summary>
+	void OnDrawGizmos()
+	{
+		Gizmos.color = lineColor;
+
+		Transform[] pathsTransform = GetComponentsInChildren<Transform>();
+		nodes = new List<Transform>();
+
+		foreach(Transform item in pathsTransform){
+			if(item!=transform){
+				nodes.Add(item);
+			}
+		}
+/*
+		Vector3 p0 = nodes[0].position;
+
+		for(int i=1; i<nodes.Count; i +=2){
+			
+			Vector3 p1 = nodes[i].position;
+			Vector3 p2 = nodes[i+1].position;
+
+//		print(i);
+
+				for(int j=1; j<=segmentCount; j++){
+					float t = j/(float) segmentCount;
+		
+					Vector3 newPos = GetPoint(p0, p1, p2, t);
+					//print(endPos);
+				//	Gizmos.DrawWireSphere(newPos, 0.5f);
+					//Gizmos.DrawLine(p0, newPos);
+					p0 = newPos;
+				}
+
+				
+		} // loop end 
+*/
+		drawAllNodes();
+
+	} // end 
+
+	void drawAllNodes()
+	{
+			for(int i=0; i<nodes.Count; i++){
+
+			Vector3 currentNode = nodes[i].position;
+			Vector3 previousNode = Vector3.zero;
+
+				if(i>0){  // previous node 
+					previousNode = nodes[i-1].position;
+				}
+				else if(i==0 && nodes.Count>1){ // last node 
+					previousNode = nodes[nodes.Count - 1].position;
+				}
+
+				Gizmos.DrawLine(previousNode, currentNode);
+				Gizmos.DrawWireSphere(currentNode, 0.5f);
+			}
+	}
+
+
+	 Vector3 GetPoint (Vector3 p0, Vector3 p1, Vector3 p2, float t) {
+		 // B(t) = (1-t)2P0 + 2(1-t)tP1 + t2P2 , 0 < t < 1
+		t = Mathf.Clamp01(t);
+		float oneMinusT = 1f - t;
+		return
+			oneMinusT * oneMinusT * p0 +
+			2f * oneMinusT * t * p1 +
+			t * t * p2;
+	}
+
+}
---
+++ Scripts/Simple Path Follower/PathFollower.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour {
+
+	public PathEditor pathEditor;
+	public float speed  = 2f;
+	public float rotSpeed = 1.5f;
+	public float reachDist = 0.1f;
+	int currentWayPointID = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float distance = Vector3.Distance(pathEditor.nodes[currentWayPointID].position, transform.position);
+		//print(distance);
+		// move obj
+		transform.position = Vector3.MoveTowards(transform.position, pathEditor.nodes[currentWayPointID].position, Time.deltaTime * speed);
+
+		if(distance<=reachDist){
+			currentWayPointID++;
+		}
+		if(currentWayPointID>=pathEditor.nodes.Count){
+			currentWayPointID = 0;
+		}
+
+		Vector3 dir = (pathEditor.nodes[currentWayPointID].position - transform.position).normalized;
+		Quaternion lookRot = Quaternion.LookRotation(dir);
+		Quaternion targetRot = Quaternion.Euler(0, lookRot.y, 0) * transform.rotation;
+		//transform.rotation = lookRot;
+		transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime*rotSpeed);
+	//transform.rotation = Quaternion.Euler(0, rot.y, 0);
+
+	}
+
+
+	 Vector3 GetPoint (Vector3 p0, Vector3 p1, Vector3 p2, float t) {
+		 // B(t) = (1-t)2P0 + 2(1-t)tP1 + t2P2 , 0 < t < 1
+		t = Mathf.Clamp01(t);
+		float oneMinusT = 1f - t;
+		return
+			oneMinusT * oneMinusT * p0 +
+			2f * oneMinusT * t * p1 +
+			t * t * p2;
+	}
+
+
+}
