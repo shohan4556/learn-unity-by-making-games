82a03057007d17b2f677c6f9b2d34ac3480f2680
Shohanur Rahaman <shohan4556@gmail.com>
2017-12-29 23:44:00 +0600

    get and insert data from php mysql db

---
+++ Testing Mysql_PHP/Assets/DatabaseLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseLoader : MonoBehaviour {
+
+	public string Name;
+	public int ID;
+	public int Score;
+
+	public string[] allData;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		WWW itemsData = new WWW("http://localhost/Unity3D/insertData.php");
+		yield return itemsData;
+		string data = itemsData.text;
+		Debug.Log(data);
+
+		allData = data.Split(' ');
+	}
+
+
+}
---
+++ Testing Mysql_PHP/Assets/InsertDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsertDatabase : MonoBehaviour {
+
+	string url = "http://localhost/Unity3D/insertData.php";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space)){
+			insert("shohan",003,5000);
+		}	
+	}
+
+	public void insert(string name, int id, int score){
+		WWWForm wwwfrom = new WWWForm();
+
+		wwwfrom.AddField("idPost", id);
+		wwwfrom.AddField("namePost", name);
+		wwwfrom.AddField("scorePost", score);
+
+		WWW www = new WWW(url, wwwfrom);
+	}
+
+}
