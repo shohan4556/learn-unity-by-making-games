4831cbf3dc10cb82fa599b21001d51a60b8b21ad
Shohanur Rahaman <shohan4556@gmail.com>
2019-11-17 13:08:28 +0600

    Add files via upload

---
+++ Scripts/RaycastDebug.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastDebug : MonoBehaviour {
+    private Vector3 _forwar;
+    private Vector3 _backward;
+    private Vector3 _left;
+    private Vector3 _right;
+    private float rayLength = 2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        _forwar = transform.forward;
+        _backward = -transform.forward;
+        _right = transform.right;
+        _left = -transform.right;
+    }
+
+    private void OnDrawGizmosSelected() {
+        Debug.DrawRay(this.transform.position, _forwar * rayLength, Color.blue);
+        Debug.DrawRay(this.transform.position, _backward * rayLength);
+        Debug.DrawRay(this.transform.position, _left * rayLength);
+        Debug.DrawRay(this.transform.position, _right * rayLength);
+    }
+}
