4860de9bf76172f20096196f4c70820c3237b74c
Shohanur Rahaman <shohan4556@gmail.com>
2020-06-22 12:17:18 +0600

    Add files via upload

---
+++ Scripts/ScaleSprite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleSprite : MonoBehaviour
+{
+    public float multiplier = 2f;
+    private float cameraHeight;
+    private float cameraWidth;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraHeight = Camera.main.orthographicSize;
+        cameraWidth = cameraHeight * Screen.width / Screen.height; // cameraHeight * aspect ratio
+        
+    }
+
+    private void Update() {
+        gameObject.transform.localScale = Vector3.one * cameraWidth / multiplier;
+    }
+}
